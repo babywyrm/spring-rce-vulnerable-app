d483a6835b1737742bd0b81be0dbd7d14b88926d
Free Wortley <free@lunasec.io>
2022-03-30 04:47:05 -0500

    Add POST endpoint for making unicode payloads easier to pass through

--- src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
+++ src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
@@ -1,9 +1,7 @@
 package fr.christophetd.log4shell.vulnerableapp;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.util.SerializationUtils;
 
 import org.apache.logging.log4j.LogManager;
@@ -20,7 +18,18 @@ public class MainController {
         logger.info("Received a request for API version " + apiVersion);
         
         // A payload that is capable of triggering a Java RCE on deserialization would be able to fire here.
-        Object deserialized = (Object) SerializationUtils.deserialize(SerializationUtils.serialize(apiVersion));
+        Object deserialized = SerializationUtils.deserialize(SerializationUtils.serialize(apiVersion));
+
+        return "Hello, " + deserialized;
+    }
+
+    @PostMapping("/post-body")
+    public String postBody(@RequestBody String body) {
+
+        logger.info("Received a request with body " + body);
+
+        // A payload that is capable of triggering a Java RCE on deserialization would be able to fire here.
+        Object deserialized = SerializationUtils.deserialize(SerializationUtils.serialize(body));
 
         return "Hello, " + deserialized;
     }
